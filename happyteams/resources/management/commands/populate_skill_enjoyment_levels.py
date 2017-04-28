222e17bd9b7a7a3e2bd588b2d37b0a5afba335d8
Danny Browne <daniel.browne@gtri.gatech.edu>
2017-04-27 21:08:10 -0400

    added requirements.txt and custom command to add fixture data

---
+++ happyteams/resources/management/commands/populate_skill_enjoyment_levels.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from resources.models import SkillEnjoyment
+
+class Command(BaseCommand):
+    help = ('Populates the database with a suggested set of '
+            'Skill Enjoyment Levels.')
+
+    def handle(self, *args, **options):
+        enjoyment_levels = [
+            ('None', 0, 'Please don\'t make me do this.'),
+            ('Little', 1, 'Not my favorite but I\'ll do it if needed.'),
+            ('Enjoy', 4, 'I enjoy doing this.'),
+            ('Favorite', 9, 'This is (one of) my favorite part(s) of my job.'),
+        ]
+        for enjoyment_level in enjoyment_levels:
+            SkillEnjoyment.objects.create(
+                slug=enjoyment_level[0],
+                value=enjoyment_level[1],
+                description=enjoyment_level[2]
+            )
+        
+        self.stdout.write(
+            self.style.SUCCESS('Successfully loaded Skill Levels.')
+        )
