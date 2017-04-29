6646c7beafe57d2335143c4aa2c8912c3e3b3bb2
Santiago Balestrini-Robinson <santiago.balestrini@gtri.gatech.edu>
2017-04-29 13:12:34 -0400

    added auto registering of admin models for initial development

--- happyteams/planning/admin.py
+++ happyteams/planning/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.apps import apps
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(apps.get_app_config('planning').models.values(), admin.ModelAdmin)
